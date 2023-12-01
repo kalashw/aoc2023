e76d69cbb93cae1ab48b424b4098bfab3e7668d6
vladyslav.kalashnyk <vladyslav.kalashnyk@fullscript.com>
2023-12-01 08:51:48 -0500

    lint because you write ugly without it

--- boilerday/main.js
+++ boilerday/main.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-const _ = require('lodash')
+const _ = require('lodash'); //
 
-const json = JSON.parse(fs.readFileSync("./input.json").toString());
-const text = fs.readFileSync("./input.txt").toString();
-
+const json = JSON.parse(fs.readFileSync('./input.json').toString());
+const text = fs.readFileSync('./input.txt').toString();
 
 console.log(json);
 console.log(text);
+console.log(_.reverse(Array.from(text)).toString());
--- day1/main.js
+++ day1/main.js
@@ -1,29 +1,29 @@
 const fs = require('fs');
-const _ = require('lodash')
+const _ = require('lodash');
 
-//part 1
-const lines = fs.readFileSync("./input.txt").toString().split("\n");
+// part 1
+const lines = fs.readFileSync('./input.txt').toString().split('\n');
 
 const processLine = line => {
-    const first = _.find(line, a => ("0" <= a) && (a <= "9"));
-    const last = _.findLast(line, a => ("0" <= a) && (a <= "9"));
+    const first = _.find(line, a => (a >= '0') && (a <= '9'));
+    const last = _.findLast(line, a => (a >= '0') && (a <= '9'));
     return first * 10 + (last * 1);
 };
 
-console.log("Part 1:", _.sum(lines.map(processLine)));
+console.log('Part 1:', _.sum(lines.map(processLine)));
 
-//part 2
+// part 2
 const list = {
-    "0": 0,
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
     zero: 0,
     one: 1,
     two: 2,
@@ -34,16 +34,16 @@ const list = {
     seven: 7,
     eight: 8,
     nine: 9,
-}
-const part2 = fs.readFileSync("./input.txt").toString().split("\n").map((line) => {
+};
+const part2 = fs.readFileSync('./input.txt').toString().split('\n').map(line => {
     const firstNum = _.sortBy(
         Object.keys(list)
             .map(item => ({
                 name: item,
                 value: line.indexOf(item),
             }))
-            .filter(x => x.value != -1),
-        'value'
+            .filter(x => x.value !== -1),
+        'value',
     )[0];
 
     const reversedLine = _.reverse(Array.from(line)).toString();
@@ -53,11 +53,11 @@ const part2 = fs.readFileSync("./input.txt").toString().split("\n").map((line) =
                 name: item,
                 value: reversedLine.indexOf(_.reverse(Array.from(item))),
             }))
-            .filter(x => x.value != -1),
+            .filter(x => x.value !== -1),
         'value',
-        )[0];
+    )[0];
 
     return list[firstNum.name] * 10 + list[lastNum.name];
 });
 
-console.log("Part 2:", _.sum(part2));
+console.log('Part 2:', _.sum(part2));
