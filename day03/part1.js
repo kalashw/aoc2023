22c45b6139fa60f85a46e57d9a606340d1f35d95
vladyslav.kalashnyk <vladyslav.kalashnyk@fullscript.com>
2023-12-03 01:11:28 -0500

    day03 what a terrible brute solution :(

---
+++ day03/part1.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const _ = require('lodash'); //
+
+const lines = fs.readFileSync('./input.txt').toString().split('\n');
+
+const isNumber = a => ((a >= '0') && a <= '9');
+const isSymbol = a => !isNumber(a) && a !== '.';
+
+const checkTopLeft = (lineIndex, symbolIndex) => lineIndex !== 0
+    && symbolIndex !== 0
+    && isSymbol(lines[lineIndex - 1][symbolIndex - 1]);
+
+const checkTop = (lineIndex, symbolIndex) => lineIndex !== 0
+    && isSymbol(lines[lineIndex - 1][symbolIndex]);
+
+const checkTopRight = (lineIndex, symbolIndex) => lineIndex !== 0
+    && symbolIndex !== lines[lineIndex].length - 1
+    && isSymbol(lines[lineIndex - 1][symbolIndex + 1]);
+
+const checkLeft = (lineIndex, symbolIndex) => symbolIndex !== 0
+    && isSymbol(lines[lineIndex][symbolIndex - 1]);
+
+const checkRight = (lineIndex, symbolIndex) => symbolIndex !== lines[lineIndex].length - 1
+    && isSymbol(lines[lineIndex][symbolIndex + 1]);
+
+const checkBottomLeft = (lineIndex, symbolIndex) => lineIndex !== lines.length - 1
+    && symbolIndex !== 0
+    && isSymbol(lines[lineIndex + 1][symbolIndex - 1]);
+
+const checkBottom = (lineIndex, symbolIndex) => lineIndex !== lines.length - 1
+    && isSymbol(lines[lineIndex + 1][symbolIndex]);
+
+const checkBottomRight = (lineIndex, symbolIndex) => lineIndex !== lines.length - 1
+    && symbolIndex !== lines[lineIndex].length - 1
+    && isSymbol(lines[lineIndex + 1][symbolIndex + 1]);
+
+const isTouchedByStar = (lineIndex, symbolIndex) => checkTopLeft(lineIndex, symbolIndex)
+        || checkTop(lineIndex, symbolIndex)
+        || checkTopRight(lineIndex, symbolIndex)
+        || checkLeft(lineIndex, symbolIndex)
+        || checkRight(lineIndex, symbolIndex)
+        || checkBottomLeft(lineIndex, symbolIndex)
+        || checkBottom(lineIndex, symbolIndex)
+        || checkBottomRight(lineIndex, symbolIndex);
+
+let sum = 0;
+
+const parseLine = lineIndex => {
+    const line = lines[lineIndex];
+    let currentNumber = 0;
+    let currentNumberIsTouched = false;
+    _.each(line, (letter, symbolIndex) => {
+        if (isNumber(letter)) {
+            currentNumber = +currentNumber * 10 + +letter;
+            currentNumberIsTouched = currentNumberIsTouched
+                || isTouchedByStar(lineIndex, symbolIndex);
+        } else if (currentNumberIsTouched) {
+            sum += +currentNumber;
+            currentNumber = 0;
+            currentNumberIsTouched = false;
+        } else {
+            currentNumber = 0;
+            currentNumberIsTouched = false;
+        }
+    });
+    if (currentNumberIsTouched) {
+        sum += currentNumber;
+        currentNumber = 0;
+        currentNumberIsTouched = false;
+    }
+};
+
+lines.forEach((line, index) => parseLine(index));
+
+console.log('Part 1:', sum);
---
+++ day03/part2.js
@@ -0,0 +1,97 @@
+const fs = require('fs');
+const _ = require('lodash'); //
+
+const lines = fs.readFileSync('./input.txt').toString().split('\n');
+
+const isNumber = a => ((a >= '0') && a <= '9');
+const isStar = a => a === '*';
+
+const checkTopLeft = (lineIndex, symbolIndex) => lineIndex !== 0
+    && symbolIndex !== 0
+    && isStar(lines[lineIndex - 1][symbolIndex - 1]);
+
+const checkTop = (lineIndex, symbolIndex) => lineIndex !== 0
+    && isStar(lines[lineIndex - 1][symbolIndex]);
+
+const checkTopRight = (lineIndex, symbolIndex) => lineIndex !== 0
+    && symbolIndex !== lines[lineIndex].length - 1
+    && isStar(lines[lineIndex - 1][symbolIndex + 1]);
+
+const checkLeft = (lineIndex, symbolIndex) => symbolIndex !== 0
+    && isStar(lines[lineIndex][symbolIndex - 1]);
+
+const checkRight = (lineIndex, symbolIndex) => symbolIndex !== lines[lineIndex].length - 1
+    && isStar(lines[lineIndex][symbolIndex + 1]);
+
+const checkBottomLeft = (lineIndex, symbolIndex) => lineIndex !== lines.length - 1
+    && symbolIndex !== 0
+    && isStar(lines[lineIndex + 1][symbolIndex - 1]);
+
+const checkBottom = (lineIndex, symbolIndex) => lineIndex !== lines.length - 1
+    && isStar(lines[lineIndex + 1][symbolIndex]);
+
+const checkBottomRight = (lineIndex, symbolIndex) => lineIndex !== lines.length - 1
+    && symbolIndex !== lines[lineIndex].length - 1
+    && isStar(lines[lineIndex + 1][symbolIndex + 1]);
+
+const isTouched = (lineIndex, symbolIndex) => {
+    const stars = [];
+    if (checkTopLeft(lineIndex, symbolIndex)) stars.push([lineIndex - 1, symbolIndex - 1]);
+    if (checkTop(lineIndex, symbolIndex)) stars.push([lineIndex - 1, symbolIndex]);
+    if (checkTopRight(lineIndex, symbolIndex)) stars.push([lineIndex - 1, symbolIndex + 1]);
+    if (checkLeft(lineIndex, symbolIndex)) stars.push([lineIndex, symbolIndex - 1]);
+    if (checkRight(lineIndex, symbolIndex)) stars.push([lineIndex, symbolIndex + 1]);
+    if (checkBottomLeft(lineIndex, symbolIndex)) stars.push([lineIndex + 1, symbolIndex - 1]);
+    if (checkBottom(lineIndex, symbolIndex)) stars.push([lineIndex + 1, symbolIndex]);
+    if (checkBottomRight(lineIndex, symbolIndex)) stars.push([lineIndex + 1, symbolIndex + 1]);
+
+    return stars;
+};
+
+const touchedStars = {};
+
+const mapTouchedStars = (stars, num) => _.chain(stars)
+    .map(([lI, sI]) => `${lI}_${sI}`)
+    .uniq()
+    .each(hash => {
+        if (!touchedStars[hash]) {
+            touchedStars[hash] = {
+                count: 1,
+                prod: num,
+            };
+        } else {
+            touchedStars[hash] = {
+                count: touchedStars[hash].count + 1,
+                prod: touchedStars[hash].prod * num,
+            };
+        }
+    })
+    .value();
+
+const parseLine = lineIndex => {
+    const line = lines[lineIndex];
+    let currentNumber = 0;
+    let currentTouchedStars = [];
+    _.each(line, (letter, symbolIndex) => {
+        if (isNumber(letter)) {
+            currentNumber = +currentNumber * 10 + +letter;
+            currentTouchedStars = [...currentTouchedStars,
+                ...isTouched(lineIndex, symbolIndex)];
+        } else {
+            mapTouchedStars(currentTouchedStars, currentNumber);
+            currentNumber = 0;
+            currentTouchedStars = [];
+        }
+    });
+    mapTouchedStars(currentTouchedStars, currentNumber);
+};
+
+lines.map((line, index) => parseLine(index));
+
+const answer = _.chain(touchedStars)
+    .filter({ count: 2 })
+    .map('prod')
+    .sum()
+    .value();
+
+console.log('Part 2:', answer);
