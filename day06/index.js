f602c7fdd4fda1851b7c9f7c57b813792a28a5cd
vladyslav.kalashnyk <vladyslav.kalashnyk@fullscript.com>
2023-12-06 01:31:59 -0500

    day06 fluff

--- day06/index.js
+++ day06/index.js
@@ -12,18 +12,18 @@ const distances = _.chain(text[1].substring(10).split(' '))
     .map(_.toNumber)
     .value();
 
-const calculateWinners = (time, distance) => {
+const countWinnerWaitTimes = (time, distance) => {
     const lowerRoot = 0.5 * (time - Math.sqrt(time * time - 4 * distance));
-    const integerLower = Math.floor(lowerRoot + 1);
+    const lowestPossibleWaitTime = Math.floor(lowerRoot + 1);
 
     const higherRoot = 0.5 * (time + Math.sqrt(time * time - 4 * distance));
-    const integerHigher = Math.ceil(higherRoot - 1);
+    const highestPossibleWaitTime = Math.ceil(higherRoot - 1); // highest possible wait time
 
-    return integerHigher - integerLower + 1;
+    return highestPossibleWaitTime - lowestPossibleWaitTime + 1;
 };
 
 const answer = _.zip(times, distances)
-    .map(([time, distance]) => calculateWinners(time, distance))
+    .map(([time, distance]) => countWinnerWaitTimes(time, distance))
     .reduce((val, product) => product * val, 1);
 
 console.log('Part 1', answer);
@@ -31,4 +31,4 @@ console.log('Part 1', answer);
 const times2 = +times.join('');
 const distance2 = +distances.join('');
 
-console.log('Part 2', calculateWinners(times2, distance2));
+console.log('Part 2', countWinnerWaitTimes(times2, distance2));
