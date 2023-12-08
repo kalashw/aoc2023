d2a0d59553a0973099739a4f1b621d6dcca7dcfc
vladyslav.kalashnyk <vladyslav.kalashnyk@fullscript.com>
2023-12-07 20:01:13 -0500

    eslintignore

--- boilerday/index.js
+++ boilerday/index.js
@@ -1,7 +1,4 @@
 const fs = require('fs');
 const _ = require('lodash');
 
-const text = fs.readFileSync('./input.txt').toString().split('\n');
-
-console.log(text);
-console.log(_.reverse(Array.from(text)).toString());
+const lines = fs.readFileSync('./input.txt').toString().split('\n');
