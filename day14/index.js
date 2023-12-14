0cacabebc08cdd2949ed6a989982ffb4c172978b
vladyslav.kalashnyk <vladyslav.kalashnyk@fullscript.com>
2023-12-14 01:40:35 -0500

    day14 cycling

---
+++ day14/index.js
@@ -0,0 +1,104 @@
+const fs = require('fs');
+const _ = require('lodash');
+
+const lines = fs.readFileSync('./input.txt').toString().split('\n').map(line => line.split(''));
+
+// part 1
+const getLineWeight = line => {
+    let lastPosition = -1;
+    let sum = 0;
+    line.forEach((elem, index) => {
+        if (elem === 'O') {
+            lastPosition += 1;
+            sum += line.length - lastPosition;
+        } else if (elem === '#') {
+            lastPosition = index;
+        }
+    });
+    return sum;
+};
+const rotateL = hash => hash[0].map((val, index) => hash.map(row => row[row.length - 1 - index]));
+const rotateR = hash => hash[0].map((val, index) => hash.map(row => row[index]).reverse());
+
+const answer = _.chain(rotateL(lines))
+    .map(getLineWeight)
+    .sum()
+    .value();
+
+console.log('Part 1:', answer);
+
+// part 2
+
+const calculateLine = line => {
+    let lastPosition = -1;
+    const result = Array(line.length);
+    line.forEach((elem, index) => {
+        if (elem === 'O') {
+            lastPosition += 1;
+            result[lastPosition] = 'O';
+        } else if (elem === '#') {
+            _.fill(result, '.', lastPosition + 1, index);
+            result[index] = '#';
+            lastPosition = index;
+        } else if (lastPosition === -1) {
+            result[index] = '.';
+        }
+    });
+    _.fill(result, '.', lastPosition + 1, line.length);
+    return result;
+};
+
+const cycleThroughHash = hash => {
+    // north cycle
+    const nResult = rotateR(hash).map(calculateLine);
+
+    // west cycle
+    const wResult = rotateR(nResult).map(calculateLine);
+
+    // south cycle
+    const sResult = rotateR(wResult).map(calculateLine);
+
+    // east cycle
+    return rotateR(sResult).map(calculateLine);
+};
+
+let tableToCycle = rotateL(rotateL(lines));
+const resultSums = {};
+
+let dontStop = true;
+let index = 0;
+let cycleStart;
+let cycleEnd;
+
+while (dontStop) {
+    const result = cycleThroughHash(tableToCycle);
+
+    const answ = _.chain(result)
+        .map((line, ind) => (ind + 1) * (line.join('').split('O').length - 1))
+        .sum()
+        .value();
+
+    const hashedTable = result.map(line => line.join('')).join('\n');
+
+    if (resultSums[hashedTable]) {
+        dontStop = false;
+        cycleStart = resultSums[hashedTable].index;
+        cycleEnd = index;
+    } else {
+        resultSums[hashedTable] = {
+            index,
+            answ,
+        };
+    }
+
+    tableToCycle = result;
+    index += 1;
+}
+
+const answerIndex = ((1000000000 - cycleStart - 1) % (cycleEnd - cycleStart)) + cycleStart;
+const answer2 = _.find(
+    resultSums,
+    { index: answerIndex },
+).answ;
+
+console.log('Part 2:', answer2);
