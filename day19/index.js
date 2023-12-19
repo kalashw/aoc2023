21c99061937f5832605bf7974fed1434d8255e3c
vladyslav.kalashnyk <vladyslav.kalashnyk@fullscript.com>
2023-12-19 01:43:46 -0500

    day19 was somehow easy?

--- day19/index.js
+++ day19/index.js
@@ -39,6 +39,7 @@ const parseInstruction = instructionLine => {
     return [instructionName, parsedInstructions];
 };
 
+// part 1
 const checkCondition = ({ conditionType, value, conditionLetter }, part) => {
     if (conditionType === 'End') {
         return true;
@@ -77,14 +78,77 @@ const findAllAcceptedParts = () => {
     }));
 };
 
-// part 1
 const answer = _.chain(findAllAcceptedParts())
     .map(part => _.sum(_.values(part)))
     .sum()
     .value();
+
 console.log('Part 1:', answer);
 
 // part 2
+const applyInstructions2 = ({
+    instructionName, conditionIndex, part, instructions,
+}) => {
+    if (instructionName === 'A') {
+        return {
+            isAccepted: true,
+            part,
+        };
+    }
+    if (instructionName === 'R') {
+        return {
+            isAccepted: false,
+            part,
+        };
+    }
+
+    if (_.some(part, interval => interval[0] + 1 >= interval[1])) {
+        return {
+            isAccepted: false,
+            part,
+        };
+    }
+    const {
+        conditionType, conditionLetter, next, value,
+    } = instructions[instructionName][conditionIndex];
+
+    if (conditionType === 'End') {
+        return applyInstructions2({
+            instructionName: next,
+            conditionIndex: 0,
+            part,
+            instructions,
+        });
+    }
+
+    const newPartTrue = { ...part };
+    const newPartFalse = { ...part };
+    const [start, end] = part[conditionLetter];
+
+    if (conditionType === '>') {
+        newPartTrue[conditionLetter] = [_.max([start, value]), end];
+
+        newPartFalse[conditionLetter] = [start, _.min([value + 1, end])];
+    } else {
+        newPartTrue[conditionLetter] = [start, _.min([end, value])];
+        newPartFalse[conditionLetter] = [_.max([start, value - 1]), end];
+    }
+
+    return _.flatten([
+        applyInstructions2({
+            part: newPartTrue,
+            instructionName: next,
+            conditionIndex: 0,
+            instructions,
+        }),
+        applyInstructions2({
+            part: newPartFalse,
+            instructionName,
+            conditionIndex: conditionIndex + 1,
+            instructions,
+        }),
+    ]);
+};
 
 const findAllPossibleAcceptedParts = () => {
     const instructions = _.fromPairs(instructionLines.map(parseInstruction));
@@ -95,69 +159,11 @@ const findAllPossibleAcceptedParts = () => {
         s: [0, 4001],
     };
 
-    const applyInstructions2 = ({ instructionName, conditionIndex, part }) => {
-        if (instructionName === 'A') {
-            return {
-                isAccepted: true,
-                part,
-            };
-        }
-        if (instructionName === 'R') {
-            return {
-                isAccepted: false,
-                part,
-            };
-        }
-
-        if (_.some(part, interval => interval[0] + 1 >= interval[1])) {
-            return {
-                isAccepted: false,
-                part,
-            };
-        }
-        const {
-            conditionType, conditionLetter, next, value,
-        } = instructions[instructionName][conditionIndex];
-
-        if (conditionType === 'End') {
-            return applyInstructions2({
-                instructionName: next,
-                conditionIndex: 0,
-                part,
-            });
-        }
-
-        const newPartTrue = { ...part };
-        const newPartFalse = { ...part };
-        const [start, end] = part[conditionLetter];
-
-        if (conditionType === '>') {
-            newPartTrue[conditionLetter] = [_.max([start, value]), end];
-
-            newPartFalse[conditionLetter] = [start, _.min([value + 1, end])];
-        } else {
-            newPartTrue[conditionLetter] = [start, _.min([end, value])];
-            newPartFalse[conditionLetter] = [_.max([start, value - 1]), end];
-        }
-
-        return _.flatten([
-            applyInstructions2({
-                part: newPartTrue,
-                instructionName: next,
-                conditionIndex: 0,
-            }),
-            applyInstructions2({
-                part: newPartFalse,
-                instructionName,
-                conditionIndex: conditionIndex + 1,
-            }),
-        ]);
-    };
-
     return applyInstructions2({
         instructionName: 'in',
         conditionIndex: 0,
         part: startingPart,
+        instructions,
     })
         .filter(({ isAccepted }) => isAccepted);
 };
